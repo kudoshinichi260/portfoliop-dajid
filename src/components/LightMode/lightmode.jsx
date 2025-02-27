28b7e37e48c42d8ab903899a5b114a22ca5ffac9
kudoshinichi260 <nguyenhoanggiadai1412@gmail.com>
2025-02-27 23:33:01 +0700

    light mode

---
+++ src/components/LightMode/lightmode.jsx
@@ -0,0 +1,26 @@
+import React from "react";
+import "./LightMode.css";
+
+const lightmode = () => {
+    const setDarkMode = () => {
+        document.querySelector("body").setAttribute("data-theme", "dark");
+    };
+    const setLightMode = () => {
+        document.querySelector("body").setAttribute("data-theme", "light");
+    };
+    const toggleTheme = (e) => {
+        if(e.target.checked) setDarkMode();
+        else setLightMode();
+    }
+    return (
+        <div>
+            <label className="switch">
+                <input type="checkbox" 
+                        onChange={toggleTheme}/>
+                <span className="slider round"> </span>
+            </label>
+        </div>
+        
+    )
+}
+export default lightmode
